90c1b6d5f4728c6125e1e2a3de7358b7d3de4e67
Panika Yadra <panikayadra@proton.me>
2026-02-13 01:46:59 +0100

    implemented base logic for learn.html

--- static/js/script_utils.js
+++ static/js/script_utils.js
@@ -1,10 +1,27 @@
-var allScripts = [];
-
 const googleTranslateBaseUrl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&dt=t&dt=bd&dj=1";
+
+var allScripts = [];
+var wordlist = [];
+var countriesIso2 = {};
+var scriptsByCountry = {};
+
+// Load JSON files
+$.ajaxSetup({
+   	async: false
+});
 
 $.getJSON("static/scripts.json", function(json) {
 	allScripts = json;
 });
+
+$.getJSON("static/countries_iso2.json", function(json) {
+	countriesIso2 = json;
+});
+
+$.getJSON("static/wordlist.json", function(json) {
+	wordlist = json;
+});
+
 
 function translateSentence(destScript, sentence) {
 	var destLang = destScript;
